55a27aaba7e5a2f3dd85770b12e9133a0312d765
damlaSub <96023986+damlaSub@users.noreply.github.com>
2025-08-18 21:47:16 +0200

    Time: 2 ms (72.06%), Space: 41.7 MB (52.9%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -3,20 +3,20 @@ class Solution {
 
         Arrays.sort(citations);
         int articalNumber = citations.length;
-        int minCitation = citations[0];
+        int maxH = 0;
          
 
         for(int i=0; i< articalNumber; i++){
             int n = articalNumber-i;
             
                int temp = Math.min(n, citations[i]);
-               minCitation = Math.max(temp, minCitation);
+               maxH = Math.max(temp, maxH);
             
 
             
             
         }
-        return Math.min(minCitation, articalNumber);
+        return maxH;
 
     }
 }
